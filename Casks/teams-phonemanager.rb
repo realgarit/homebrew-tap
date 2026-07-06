3bdf1e94e55ce5197cc65e5904d62247a6a9e054
realgarit <github@realgar.ch>
2026-07-06 18:46:54 +0200

    teams-phonemanager v3.18.0

--- Casks/teams-phonemanager.rb
+++ Casks/teams-phonemanager.rb
@@ -1,12 +1,12 @@
 cask "teams-phonemanager" do
-  version "3.17.0"
+  version "3.18.0"
 
   on_arm do
-    sha256 "2c772efc494fb3eea33aa5c3fd92d4bbe7c4727715cdd5c07de1cd70cb8d8609"
+    sha256 "9493c993346b0f3f2bff314d11dfa89c2c2d1ae0421e8a2613c1ee6614c46ca9"
     url "https://github.com/realgarit/teams-phonemanager/releases/download/v#{version}/teams-phonemanager-osx-arm64.zip"
   end
   on_intel do
-    sha256 "ccad04a5704326ebf4c40891ead4e48bebe9e9eaa9311211935866a05c6d4740"
+    sha256 "ce8e3eeef229e80f0bd49e96584c1ca26cebb79ec69d583d6fc962529a0616cf"
     url "https://github.com/realgarit/teams-phonemanager/releases/download/v#{version}/teams-phonemanager-osx-x64.zip"
   end
 
