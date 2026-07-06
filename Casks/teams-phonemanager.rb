bc89d2b94d5ee8d71ceab926007d2b0d9d0820ed
realgarit <github@realgar.ch>
2026-07-06 18:19:35 +0200

    teams-phonemanager v3.17.0

---
+++ Casks/teams-phonemanager.rb
@@ -0,0 +1,24 @@
+cask "teams-phonemanager" do
+  version "3.17.0"
+
+  on_arm do
+    sha256 "2c772efc494fb3eea33aa5c3fd92d4bbe7c4727715cdd5c07de1cd70cb8d8609"
+    url "https://github.com/realgarit/teams-phonemanager/releases/download/v#{version}/teams-phonemanager-osx-arm64.zip"
+  end
+  on_intel do
+    sha256 "ccad04a5704326ebf4c40891ead4e48bebe9e9eaa9311211935866a05c6d4740"
+    url "https://github.com/realgarit/teams-phonemanager/releases/download/v#{version}/teams-phonemanager-osx-x64.zip"
+  end
+
+  name "Teams Phone Manager"
+  desc "Microsoft Teams Phone System administration made simple"
+  homepage "https://github.com/realgarit/teams-phonemanager"
+
+  depends_on macos: ">= :monterey"
+
+  app "Teams Phone Manager.app"
+
+  zap trash: [
+    "~/Library/Preferences/ch.realgar.teams-phonemanager.plist",
+  ]
+end
